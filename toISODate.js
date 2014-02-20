8745e252d1312c80a8106afc81ee2a5a1ef5e60e
Howard Tang <aychtang@gmail.com>
2014-02-20 12:06:29 +0000

    Use lower case date.

--- toISODate.js
+++ toISODate.js
@@ -1,6 +1,5 @@
-
 // When passed a native Date object, should return
 // a string in the format of "YYYY-MM-DD".
-module.exports = function(Date) {
-	return Date.toISOString().split('T')[0];
+module.exports = function(date) {
+	return date.toISOString().split('T')[0];
 };
